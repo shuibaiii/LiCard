a08e506c50bf6487084ef62a90f24fd8afc139d6
shuibaiii <yaobx19@mails.tsinghua.edu.cn>
2022-08-13 00:41:24 +0800

    analyse the prob. of two cards relationship

--- card_relationship_function.py
+++ card_relationship_function.py
@@ -17,27 +17,33 @@ def judge_win_for_two_cards(card_1: Card, card_2: Card):
             return 0
 
 
-def judge_relationship_for_two_cards(card_1: Card, card_2: Card):
+def judge_relationship_in_structure_for_two_cards(card_1: Card, card_2: Card):
     same_char_num = get_the_num_of_same_char(card_1, card_2)
     if same_char_num == 3:
-        return '相同牌'
+        # '相同牌'
+        return 3
     elif same_char_num == 2:
-        return '强姊妹牌'
+        # 强姊妹牌
+        return 2
     elif same_char_num == 1:
-        return '弱姊妹牌'
+        # 弱姊妹牌
+        return 1
     else:
-        if check_if_same_family(card_1, card_2):
-            return '同系牌'
-        else:
-            return '无关牌'
+        return 0
 
 
-def check_if_same_family(card_1: Card, card_2: Card):
-    # 检查两张牌是否是同系牌
+def judge_relationship_in_content_for_two_cards(card_1: Card, card_2: Card):
     if card_1.family_number == card_2.family_number:
-        return True
+        # 是同系牌
+        return 1
     else:
-        return False
+        # 不是同系牌
+        return 0
+
+
+def judge_relationship_for_two_cards(card_1: Card, card_2: Card):
+    return judge_relationship_in_structure_for_two_cards(card_1, card_2), \
+           judge_relationship_in_content_for_two_cards(card_1, card_2)
 
 
 def check_if_strong_sister(card_1: Card, card_2: Card):
--- zhajinhua_2cards.py
+++ zhajinhua_2cards.py
@@ -1,8 +1,10 @@
 from card_list import CardList
-from card_relationship_function import check_if_same_family, get_the_num_of_same_char, \
-    judge_win_for_two_cards, judge_relationship_for_two_cards
+from card_relationship_function import get_the_num_of_same_char, \
+    judge_win_for_two_cards, judge_relationship_for_two_cards, \
+    judge_relationship_in_content_for_two_cards
 from settings import Settings
 import random
+import numpy as np
 
 
 # 两张牌的炸金花
@@ -32,9 +34,11 @@ def judge_win_for_two_card_lists(card_1: CardList, card_2: CardList):
     elif same_char_num_1 < same_char_num_2:
         return -1
     else:
-        if check_if_same_family(card_1[0], card_1[1]) and not check_if_same_family(card_2[0], card_2[1]):
+        if judge_relationship_in_content_for_two_cards(card_1[0], card_1[1]) and \
+                not judge_relationship_in_content_for_two_cards(card_2[0], card_2[1]):
             return 1
-        elif not check_if_same_family(card_1[0], card_1[1]) and check_if_same_family(card_2[0], card_2[1]):
+        elif not judge_relationship_in_content_for_two_cards(card_1[0], card_1[1]) and \
+                judge_relationship_in_content_for_two_cards(card_2[0], card_2[1]):
             return -1
         else:
             return judge_win_for_two_cards(card_1.get_max_card(), card_2.get_max_card())
@@ -78,11 +82,10 @@ if __name__ == '__main__':
     # 自动化测试
     settings = Settings()
     all_name_list = settings.get_all_name_list()
+    all_name_list = all_name_list + all_name_list
     the_number_of_all_name = len(all_name_list)
     print(the_number_of_all_name)
-    # print(all_name_list)
-    # print(len(all_name_list))
-    tests_number = 100000
+    tests_number = 10240000
     same_number = 0
     strong_number = 0
     weak_number = 0
@@ -91,58 +94,71 @@ if __name__ == '__main__':
     win_number = 0
     lose_number = 0
     equal_number = 0
-    with open('zhajinhua_2cards_result.txt', 'w', encoding='UTF-8') as f:
-        for i in range(tests_number):
-            # random.randint(1, 10) # 产生[1, 10]的整数随机数
-            name_list_1 = [all_name_list[random.randint(0, the_number_of_all_name-1)],
-                           all_name_list[random.randint(0, the_number_of_all_name-1)]]
-            name_list_2 = [all_name_list[random.randint(0, the_number_of_all_name-1)],
-                           all_name_list[random.randint(0, the_number_of_all_name-1)]]
-            card_list_1 = CardList(name_list_1)
-            card_list_2 = CardList(name_list_2)
-            relationship_1 = judge_relationship_for_two_cards(card_list_1[0], card_list_1[1])
-            if relationship_1 == '相同牌':
-                same_number += 1
-            elif relationship_1 == '强姊妹牌':
-                strong_number += 1
-            elif relationship_1 == '弱姊妹牌':
-                weak_number += 1
-            elif relationship_1 == '同系牌':
-                family_number += 1
-            else:
-                independent_number += 1
-            relationship_2 = judge_relationship_for_two_cards(card_list_2[0], card_list_2[1])
-            if relationship_2 == '相同牌':
-                same_number += 1
-            elif relationship_2 == '强姊妹牌':
-                strong_number += 1
-            elif relationship_2 == '弱姊妹牌':
-                weak_number += 1
-            elif relationship_2 == '同系牌':
-                family_number += 1
-            else:
-                independent_number += 1
-            result = judge_win_for_two_card_lists(card_list_1, card_list_2)
-            if result == 1:
-                win_number += 1
-            elif result == 0:
-                equal_number += 1
-            else:
-                lose_number += 1
-            text_to_write = str(i+1) + ': ' + generate_text_by_result(card_list_1, card_list_2, relationship_1,
-                                                                      relationship_2, result)
+    # 先测试各种牌型出现的概率
+    matrix = np.zeros([4, 2])
+    for j in range(tests_number):
+        name_iter = random.sample(range(0, the_number_of_all_name), 2)
+        card_list1 = CardList([all_name_list[name_iter[0]], all_name_list[name_iter[1]]])
+        x, y = judge_relationship_for_two_cards(card_list1[0], card_list1[1])
+        matrix[x][y] = matrix[x][y] + 1
+    pass
+    matrix = matrix / tests_number
+    print(matrix)
+    print(matrix.sum())
 
-            print(text_to_write, file=f)
-
-    with open('zhajinhua_2cards_result_analysis.txt', 'w', encoding='UTF-8') as f:
-        print('总对局次数： ' + str(tests_number), file=f)
-        print('胜局次数为： ' + str(win_number) + ' 比例为：' + str(win_number / tests_number), file=f)
-        print('平局次数为： ' + str(equal_number) + ' 比例为：' + str(equal_number / tests_number), file=f)
-        print('负局次数为： ' + str(lose_number) + ' 比例为：' + str(lose_number / tests_number), file=f)
-        print('', file=f)
-        print('总手牌数为：' + str(tests_number*2), file=f)
-        print('相同牌出现次数： ' + str(same_number) + ' 比例为：' + str(same_number / tests_number / 2), file=f)
-        print('强姊妹牌出现次数： ' + str(strong_number) + ' 比例为：' + str(strong_number / tests_number / 2), file=f)
-        print('弱姊妹牌出现次数： ' + str(weak_number) + ' 比例为：' + str(weak_number / tests_number / 2), file=f)
-        print('同系牌出现次数： ' + str(family_number) + ' 比例为：' + str(family_number / tests_number / 2), file=f)
-        print('无关牌出现次数： ' + str(independent_number) + ' 比例为：' + str(independent_number / tests_number / 2), file=f)
+    # all_name_list[random.randint(0, the_number_of_all_name - 1)
+    # with open('zhajinhua_2cards_result.txt', 'w', encoding='UTF-8') as f:
+    #     for i in range(tests_number):
+    #         # random.randint(1, 10) # 产生[1, 10]的整数随机数
+    #         name_list_1 = [all_name_list[random.randint(0, the_number_of_all_name-1)],
+    #                        all_name_list[random.randint(0, the_number_of_all_name-1)]]
+    #         name_list_2 = [all_name_list[random.randint(0, the_number_of_all_name-1)],
+    #                        all_name_list[random.randint(0, the_number_of_all_name-1)]]
+    #         card_list_1 = CardList(name_list_1)
+    #         card_list_2 = CardList(name_list_2)
+    #         relationship_1 = judge_relationship_for_two_cards(card_list_1[0], card_list_1[1])
+    #         if relationship_1 == '相同牌':
+    #             same_number += 1
+    #         elif relationship_1 == '强姊妹牌':
+    #             strong_number += 1
+    #         elif relationship_1 == '弱姊妹牌':
+    #             weak_number += 1
+    #         elif relationship_1 == '同系牌':
+    #             family_number += 1
+    #         else:
+    #             independent_number += 1
+    #         relationship_2 = judge_relationship_for_two_cards(card_list_2[0], card_list_2[1])
+    #         if relationship_2 == '相同牌':
+    #             same_number += 1
+    #         elif relationship_2 == '强姊妹牌':
+    #             strong_number += 1
+    #         elif relationship_2 == '弱姊妹牌':
+    #             weak_number += 1
+    #         elif relationship_2 == '同系牌':
+    #             family_number += 1
+    #         else:
+    #             independent_number += 1
+    #         result = judge_win_for_two_card_lists(card_list_1, card_list_2)
+    #         if result == 1:
+    #             win_number += 1
+    #         elif result == 0:
+    #             equal_number += 1
+    #         else:
+    #             lose_number += 1
+    #         text_to_write = str(i+1) + ': ' + generate_text_by_result(card_list_1, card_list_2, relationship_1,
+    #                                                                   relationship_2, result)
+    #
+    #         print(text_to_write, file=f)
+    #
+    # with open('zhajinhua_2cards_result_analysis.txt', 'w', encoding='UTF-8') as f:
+    #     print('总对局次数： ' + str(tests_number), file=f)
+    #     print('胜局次数为： ' + str(win_number) + ' 比例为：' + str(win_number / tests_number), file=f)
+    #     print('平局次数为： ' + str(equal_number) + ' 比例为：' + str(equal_number / tests_number), file=f)
+    #     print('负局次数为： ' + str(lose_number) + ' 比例为：' + str(lose_number / tests_number), file=f)
+    #     print('', file=f)
+    #     print('总手牌数为：' + str(tests_number*2), file=f)
+    #     print('相同牌出现次数： ' + str(same_number) + ' 比例为：' + str(same_number / tests_number / 2), file=f)
+    #     print('强姊妹牌出现次数： ' + str(strong_number) + ' 比例为：' + str(strong_number / tests_number / 2), file=f)
+    #     print('弱姊妹牌出现次数： ' + str(weak_number) + ' 比例为：' + str(weak_number / tests_number / 2), file=f)
+    #     print('同系牌出现次数： ' + str(family_number) + ' 比例为：' + str(family_number / tests_number / 2), file=f)
+    #     print('无关牌出现次数： ' + str(independent_number) + ' 比例为：' + str(independent_number / tests_number / 2), file=f)
